0fbd6e4128ed3ed674f88babbe041707a744bb40
BhargavManiar <bhargav22478@gmail.com>
2020-09-05 12:37:16 +0100

    Added comments to explain the code

--- CPP/StairCase/StairCase.cpp
+++ CPP/StairCase/StairCase.cpp
@@ -2,27 +2,33 @@
 using namespace std; 
 
 int staircase(int n){
-	if(n<0){
+	// Two base cases.
+	if(n<0){ // Return 0 if a negative number
 		return 0;
 	}
 
-	if(n==0){
+	if(n==0){ // Return 1 if we get to 0
 		return 1; 
 	}
 
-	int count = 0; 
-	count += staircase(n-1);
-	count += staircase(n-2); 
-	count += staircase(n-3); 
+	int count = 0; // Declration of counter value. 
 
-	return count;
+	// Recursive calls
+	count += staircase(n-1); // number of stairs - 1 
+	count += staircase(n-2); // number of stairs - 2
+	count += staircase(n-3); // number of stairs - 3
+
+	return count; // Return count value
 }
 
 int main() {
 	int n; 
+
+	// Get user input
 	cout << "Enter number of stairs" << endl;
 	cin >> n;
-
+	
+	// Show the output to the user
 	cout << "No of ways to climb stairs are ";
 	cout << staircase(n) << endl;
 

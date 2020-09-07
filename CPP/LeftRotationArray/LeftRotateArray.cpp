4d2118ae747ea8c24838e19c36c491e7ae270991
BhargavManiar <bhargav22478@gmail.com>
2020-09-07 11:03:11 +0100

    Added code to complete the program

---
+++ CPP/LeftRotationArray/LeftRotateArray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n; // Number of arrays
+	int d; // Number of times to rotate
+
+	cout << "Enter the values for n and d" << endl;
+	cin >> n >> d; // Get the information from the user
+
+	// Add the elements to the array, based on the length of the array. 
+	int a[n]; // Define an array of length 'n'
+	for(int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	cout << "Array of elements after rotation : "
+	for(int i = 0; i < n; i++)
+	{
+		cout << a[(i+d)%n] << " ";
+	}
+
+	return 0; 
+}

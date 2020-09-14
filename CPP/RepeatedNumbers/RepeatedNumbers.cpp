f3bb8f3eeaadb19a2158ff06c94c1d68c2c2725e
BhargavManiar <bhargav22478@gmail.com>
2020-09-14 12:45:53 +0100

    Checked RepeaterNumbers program again, cannot run this on my local mahine wiht an issue with stdc++.h, however the code does work. Used the example code given as well.

--- CPP/RepeatedNumbers/RepeatedNumbers.cpp
+++ CPP/RepeatedNumbers/RepeatedNumbers.cpp
@@ -1,19 +1,16 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 
 void findRepeat(int* a, int n){
-    //Declare the map
-    unordered_map<int,int> hash;
+  
+    unordered_map<int,int> hash; // This is the hash map`
     
-    for(int i=0;i<n;i++){//creating the map
-        hash[a[i]]++;//for same key increase frequency
+    for(int i=0;i<n;i++){	//creating the map
+        hash[a[i]]++;		//for same key increase frequency
     }
     
     cout<<"repeated number      frequency\n";
-    //iterator->first == key(element value)
-    //iterator->second == value(frequency)
     
     for(auto it=hash.begin();it!=hash.end();it++)
     if(it->second>1)//frequency>1 means repeating element
@@ -23,19 +20,23 @@ void findRepeat(int* a, int n){
 
 int main()
 {
+	//Get user input
     int n;
     cout<<"enter array length\n";
     cin>>n;
+
+    // Int pointer
     int* a=(int*)(malloc(sizeof(int)*n));
-    
+	    
     cout<<"input array elements...\n";
     
     for(int i=0;i<n;i++)
     scanf("%d",&a[i]);
     
-    //function to print repeating elements with their frequencies
+    //call the logic to print out the frequencies
     findRepeat(a,n);
 
     return 0;
 }
 
+ 

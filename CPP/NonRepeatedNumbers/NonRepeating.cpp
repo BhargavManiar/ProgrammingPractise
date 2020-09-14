8277fc21fad884832161bce465df1185e96f6a98
BhargavManiar <bhargav22478@gmail.com>
2020-09-14 12:57:12 +0100

    Completed NonRepeating program

---
+++ CPP/NonRepeatedNumbers/NonRepeating.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std; 
+
+void findNonRepeat(int * a, int n) {
+	unorderedMap<int,int> hash;
+	
+	int i;
+
+	for(i = 0; i < n; i++) {
+		hash[a[i]]++;
+	}
+
+	cout << "the nonrepeating numbers are: ";
+	
+	for(auto it=hash.begin(); it!=hash.end();it++)
+	if(it->second==1) // If a value only occurs once it means that its non-repeating
+	printf("%d ",it->first);
+	printf("\n");
+}
+
+int main() {
+
+	int n; 
+	cout << "enter array length\n";
+	cin >> n;
+	int* a=(int*) (malloc(sizeof(int)*));
+
+	cout<<"input array elements...\n";
+
+	for(int i = 0; i<n; i++)
+	scanf("%d",&a[i]);
+
+	//call the logic to find the frequencies
+	findNonRepeat(a,n);
+
+	return 0;
+}

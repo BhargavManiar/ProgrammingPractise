d66c31b1fcc8ea439dc4207340f471e2083b74a8
BhargavManiar <bhargav22478@gmail.com>
2020-10-06 22:13:52 +0100

    Completed main algorithm of the program

--- CPP/Mathematical/LargeListOfPrimeNumbers/main.cpp
+++ CPP/Mathematical/LargeListOfPrimeNumbers/main.cpp
@@ -3,7 +3,30 @@ using namespace std;
 
 int listPrime(int n) {
 
-	
+	int i; 
+
+	if(n<2) {
+		cout<< "Summation not possible\n";
+	}
+
+	// If n is even we can only express the number as summations of 2s 
+	else if(n % 2 == 0) {
+		for(i = 0; i < n/2; i++) {
+			cout << 2 << " ";
+		}
+
+	}
+
+	// If n is odd 
+	else {
+		n = n - 3; // Make n even from it being odd
+		for(i = 0; i < n/2; i++) {
+                          cout << 2 << " ";
+                }
+
+		cout << 3 << " ";
+
+	}
 
 }
 

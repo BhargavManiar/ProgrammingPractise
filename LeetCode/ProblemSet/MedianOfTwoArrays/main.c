ca3ad70d5022d1537c124a1d472f1c101f46dfee
BhargavManiar <bhargav22478@gmail.com>
2021-01-01 16:43:10 +0000

    Create main.c file with skeleton of the functions including main and test and fineMedianSortedArrays

---
+++ LeetCode/ProblemSet/MedianOfTwoArrays/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size);
+
+int main()
+{
+
+	reutrn 0;
+}
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size)
+{
+	double medianValue; 
+	
+	
+	return medianValue;
+}
+
+void test(int* nums1, int* nums2)
+{
+	int sizeOne = sizeof(nums1)/sizeof(nums1[0]);
+	int sizeTwo = sizeof(nums2)/sizeof(nums2[0]);
+	
+	findMedianSortedArrays(nums1,sizeOne,nums2,sizeTwo);
+	return; 
+}

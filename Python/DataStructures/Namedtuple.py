12d21e7ab5b90cb63a77da4e3e97bece898ff3b3
BhargavManiar <bhargav22478@gmail.com>
2021-02-21 17:11:46 +0000

    Added and Completed Namedtyple.py, learnt about namedtuple and its similarities and differences to a dictionary

---
+++ Python/DataStructures/Namedtuple.py
@@ -0,0 +1,18 @@
+# Python code to demonstrate namedtuple() 
+from collections import namedtuple 
+	
+# Declaring namedtuple() 
+Student = namedtuple('Student',['name','age','DOB']) 
+	
+# Adding values 
+S = Student('Nandini','19','2541997') 
+	
+# Access using index 
+print("The Student age using index is : ") 
+print(S[1]) 
+	
+# Access using name 
+print("The Student name using keyname is : ") 
+print(S.name) 
+
+

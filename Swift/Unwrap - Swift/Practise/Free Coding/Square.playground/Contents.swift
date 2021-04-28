4736d7c2732f922935e10914639992f389a23df9
BhargavManiar <bhargav22478@gmail.com>
2021-04-28 11:21:45 +0100

    Fixed an error in Square.playground

--- Swift/Unwrap - Swift/Practise/Free Coding/Square.playground/Contents.swift
+++ Swift/Unwrap - Swift/Practise/Free Coding/Square.playground/Contents.swift
@@ -1,7 +1,7 @@
 // Write a function that accepts an integer and returns that number squared. For example, sending in the number 5 will return 25
 
 func square(x : Int) -> Int {
-    return x^2;
+    return x*x;
 }
 
 square(x: 5);

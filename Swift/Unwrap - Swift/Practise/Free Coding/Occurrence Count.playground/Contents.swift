c89971216ba7ce105f8288433b2db5b88682446f
BhargavManiar <bhargav22478@gmail.com>
2021-04-30 12:05:40 +0100

    Xcode files changed

--- Swift/Unwrap - Swift/Practise/Free Coding/Occurrence Count.playground/Contents.swift
+++ Swift/Unwrap - Swift/Practise/Free Coding/Occurrence Count.playground/Contents.swift
@@ -1,6 +1,7 @@
 // Write code that goes over this array, counting the number of time it contains the number 5, then printing that total.
 
 let numbers = [1, 5, 2, 5, 3, 5]
+
 var counter = 0;
 for i in numbers {
     

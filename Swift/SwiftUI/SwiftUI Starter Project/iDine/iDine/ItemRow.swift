17e1df0c85a474fe7593f60e8d00f4c1babe5b18
BhargavManiar <bhargav22478@gmail.com>
2021-05-04 20:44:25 +0100

    Added ItemRow.swift file

---
+++ Swift/SwiftUI/SwiftUI Starter Project/iDine/iDine/ItemRow.swift
@@ -0,0 +1,22 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Bhargav Maniar on 04/05/2021.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    let item: MenuItem
+    
+    var body: some View {
+        Text(item.name)
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow()
+    }
+}

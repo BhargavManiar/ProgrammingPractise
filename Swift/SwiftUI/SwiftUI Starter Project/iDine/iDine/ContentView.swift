d0b5dc67d403511f981b9a595ba6f48f6b8d136c
BhargavManiar <bhargav22478@gmail.com>
2021-05-04 20:21:46 +0100

    Added section view for the menu

--- Swift/SwiftUI/SwiftUI Starter Project/iDine/iDine/ContentView.swift
+++ Swift/SwiftUI/SwiftUI Starter Project/iDine/iDine/ContentView.swift
@@ -13,7 +13,12 @@ struct ContentView: View {
         NavigationView {
             List{
                 ForEach(menu) { section in
-                    Text(section.name)
+                    Section(header:
+                                Text(section.name)) {
+                        ForEach(section.items) { item in
+                            Text(item.name)
+                        }
+                    }
                 }
             }
             .navigationTitle("Menu")

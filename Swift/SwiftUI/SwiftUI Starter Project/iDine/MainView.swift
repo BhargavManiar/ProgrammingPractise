ca3d9aeba5967f80c4fc79250a8fba48639f2f6b
BhargavManiar <bhargav22478@gmail.com>
2021-05-05 11:56:35 +0100

    Added the labels and glyps for tabs

--- Swift/SwiftUI/SwiftUI Starter Project/iDine/MainView.swift
+++ Swift/SwiftUI/SwiftUI Starter Project/iDine/MainView.swift
@@ -9,12 +9,23 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("Menu", systemImage: "list.dash")
+                }
+            
+            OrderView()
+                .tabItem {
+                    Label("Order", systemImage: "square.and.pencil")
+                }
+        }
     }
 }
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
+            .environmentObject(Order())
     }
 }

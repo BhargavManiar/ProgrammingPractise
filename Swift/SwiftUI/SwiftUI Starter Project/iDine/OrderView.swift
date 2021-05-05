0e291cb2236631dcbf212789e23dba72824d017e
BhargavManiar <bhargav22478@gmail.com>
2021-05-05 11:39:03 +0100

    Added OrderView.swift file

---
+++ Swift/SwiftUI/SwiftUI Starter Project/iDine/OrderView.swift
@@ -0,0 +1,20 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by Bhargav Maniar on 05/05/2021.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView()
+    }
+}

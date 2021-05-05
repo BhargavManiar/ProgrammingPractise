165e3fe8710e84d263aeea6ef77df9122a515796
BhargavManiar <bhargav22478@gmail.com>
2021-05-05 11:46:05 +0100

    Added List Sections to the Order View

--- Swift/SwiftUI/SwiftUI Starter Project/iDine/OrderView.swift
+++ Swift/SwiftUI/SwiftUI Starter Project/iDine/OrderView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct OrderView: View {
+    @EnvironmentObject var order: Order
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination:
+                        Text("Check out")) {
+                        Text("Place Order")
+                    }
+                }
+            }
+            .navigationTitle("Order")
+            .listStyle(InsetGroupedListStyle())
+        }
+    
     }
 }
 
 struct OrderView_Previews: PreviewProvider {
     static var previews: some View {
         OrderView()
+            .environmentObject(Order())
     }
 }

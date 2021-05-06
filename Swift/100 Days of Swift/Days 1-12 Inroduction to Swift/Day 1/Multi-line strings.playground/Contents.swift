739e24c507de28c92142849c4ff4edda311901ac
BhargavManiar <bhargav22478@gmail.com>
2021-05-06 10:51:02 +0100

    Added Multi-line strings

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 1/Multi-line strings.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+// The output of the string goes over multiple lines on the stdout
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+// Here using backslashes displays the text as a single line as an output. It is useful for formatting code neatly
+var str2 = """
+This goes \
+over multiple \
+lines
+"""

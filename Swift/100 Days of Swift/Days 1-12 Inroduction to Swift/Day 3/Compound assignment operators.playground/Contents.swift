91e8ed831e8336dfc2e00fb2a01e2a5708ecb194
BhargavManiar <bhargav22478@gmail.com>
2021-05-07 00:32:59 +0100

    Added Compound assignment operators

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 3/Compound assignment operators.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+// A shorthand method to combine one operater with an assignment
+
+var score = 95
+score -= 5
+
+var quote = "The rain in Spain falls mainly on the "
+quote += "Spaniards"

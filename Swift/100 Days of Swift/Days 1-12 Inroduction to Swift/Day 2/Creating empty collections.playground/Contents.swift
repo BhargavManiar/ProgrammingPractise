6fb1ed4c1b0aefd22769091dce526653563ab67d
BhargavManiar <bhargav22478@gmail.com>
2021-05-06 21:12:20 +0100

    Added Creating empty collectoins

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 2/Creating empty collections.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+// Arrays, sets, and dictionaries are called collections
+
+// Empty dictionary
+var teams = [String: String]()
+
+teams["Paul"] = "Red"
+
+// Empty array
+var results = [Int]()
+
+// Empty sets
+var words = Set<String>()
+var numbers = Set<Int>()
+
+// Alternate way to create arrays and dictionaries, similar to above syntax
+var scores = Dictionary<String, Int>()
+var results2 = Array<Int>()

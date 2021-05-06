9eda7a4f730ba4b56d0800d276af5bf970e69d7e
BhargavManiar <bhargav22478@gmail.com>
2021-05-07 00:40:39 +0100

    Added Combining conditions

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 3/Combining conditions.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let age1 = 12
+let age2 = 21
+
+if age1 > 18 && age2 > 18 {
+    print("Both are over 18")
+}
+
+if age1 > 18 || age2 > 18 {
+    print("At least one is over 18")
+}

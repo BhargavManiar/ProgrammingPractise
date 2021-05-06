4665a8d4967e634629136e087bad6e0c8b4884b4
BhargavManiar <bhargav22478@gmail.com>
2021-05-06 11:01:50 +0100

    Added Type annotations

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 1/Type annotations.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// Inferred type
+let str = "Hello, playground"
+
+// Type annotations
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true

0c5f7c98a1f888d41f95b2f0d687cb8be396efe4
BhargavManiar <bhargav22478@gmail.com>
2021-05-06 20:44:06 +0100

    Added Arrays

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 2/Arrays.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+
+beatles[1]

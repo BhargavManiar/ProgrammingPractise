18dbeb85b0658c9b680499bb6c679ccd3f2f2ed0
BhargavManiar <bhargav22478@gmail.com>
2021-05-07 00:45:05 +0100

    Added The ternary operator

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 3/The ternary operator.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+/// Ternary operator works with three values. Checks condition in first value, if true returns second value else the third one
+
+let firstCard = 11
+let secondCard = 10
+print(firstCard == secondCard ? "Cards are the same" : "Cards are different")
+
+// The same code with regular if and else statemetns
+if firstCard == secondCard {
+    print("Cards are the same")
+} else {
+    print("Cards are different")
+}

31890211dd6219463d6da3c697a387d869dee22a
BhargavManiar <bhargav22478@gmail.com>
2021-05-06 23:58:36 +0100

    Added Enumerations

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 2/Enumerations.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+
+// Examples of strings that mean the same thing
+let result = "failure"
+let result2 = "failed"
+let result3 = "fail"
+
+// Example of enum
+
+enum Result {
+    case success
+    case failure
+}
+
+let result4 = Result.failure

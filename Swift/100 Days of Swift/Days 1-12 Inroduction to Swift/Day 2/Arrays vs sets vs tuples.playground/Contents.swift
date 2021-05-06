6638d83e3bf1d97417477de28425bd4858c12fc9
BhargavManiar <bhargav22478@gmail.com>
2021-05-06 20:57:54 +0100

    Added Arrays vs sets vs tuples

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 2/Arrays vs sets vs tuples.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+
+// Tuple
+let address = (house: 555, street: "Taylor Swift Avenue", city: "Nashville")
+
+// Set
+let set = Set(["aardvark", "astronaut", "azalea"])
+
+// Array
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"] 

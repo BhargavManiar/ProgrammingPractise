ff464676f0fe5c7c590085669768ec3cbd8cf014
BhargavManiar <bhargav22478@gmail.com>
2021-05-07 00:37:39 +0100

    Added Conditions

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 3/Conditions.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let firstCard = 11
+let secondCard = 10
+
+if firstCard + secondCard == 2 {
+    print("Aces - lucky!")
+} else if firstCard + secondCard == 21 {
+    print("Blackjack!")
+} else {
+    print("Regular cards")
+}

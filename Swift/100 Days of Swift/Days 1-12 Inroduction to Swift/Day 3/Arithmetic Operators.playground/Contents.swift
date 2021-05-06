a6bf1b5a685d1959c2f8a4e997bcbca111b59e97
BhargavManiar <bhargav22478@gmail.com>
2021-05-07 00:27:09 +0100

    Added Arithmetic Operators

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 3/Arithmetic Operators.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let firstScore = 12
+let secondScore = 4
+
+let total = firstScore + secondScore        // Add
+let difference = firstScore - secondScore   // Subtract
+
+let product = firstScore * secondScore      // Multiply
+let divieed = firstScore / secondScore      // Divide
+
+let remainder = 13 % secondScore            // Modulo

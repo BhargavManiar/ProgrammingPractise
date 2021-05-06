8c768e763a7a3d8a8711155d9db25a1b4f98a63c
BhargavManiar <bhargav22478@gmail.com>
2021-05-07 00:30:56 +0100

    Added Operator overloading

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 3/Operator overloading.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// Overloading just means an operator depends on the values it is used with
+
+// Example with integers
+let meaningOfLife = 42
+let doubleMeaning = 42 + 42
+
+// Example with strings
+let fakers = "Fakers gonna "
+let action = fakers + "fake"
+
+// Example with arrays
+let firstHalf = ["John", "Paul"]
+let secondHalf = ["George", "Ringo"]
+let beatles = firstHalf + secondHalf

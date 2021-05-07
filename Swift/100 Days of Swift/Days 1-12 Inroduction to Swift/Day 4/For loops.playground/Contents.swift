8fef5cb8b9a2086a32b6fbc35ea3eb23e08d28b3
BhargavManiar <bhargav22478@gmail.com>
2021-05-07 08:58:48 +0100

    Added For loops

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 4/For loops.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// Example of a range of numbers
+let count = 1...10
+
+for number in count {
+    print("Number is \(number)")
+}
+
+let albums = ["Red", "1989", "Reputation"]
+
+for album in albums {
+    print("\(album) is on Apple Music")
+}

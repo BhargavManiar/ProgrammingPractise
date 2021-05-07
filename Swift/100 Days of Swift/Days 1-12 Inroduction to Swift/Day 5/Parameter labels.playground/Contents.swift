4131f4fd87821bb881f5720942fa28db30a81c9a
BhargavManiar <bhargav22478@gmail.com>
2021-05-07 13:25:00 +0100

    Added Parameter labels

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 5/Parameter labels.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// Swift lets us provide two names for a parameter one for external use and one for internal.
+
+//  Here is a normal example
+func square(number: Int) -> Int {
+    return number * number
+}
+
+let result = square(number: 8)
+
+// Here is an example with labels for external use as well.
+func SayHello(to name: String) {
+    print("Hello, \(name)!")
+}
+
+SayHello(to: "Taylor")

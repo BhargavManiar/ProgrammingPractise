79b2db7f6edb7c937a902379f5ce7d6c19353ef0
BhargavManiar <bhargav22478@gmail.com>
2021-05-08 14:59:36 +0100

    Added Properties - methods of strings

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 8/Properties - methods of strings .playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// String are structs, they have their own methods and properties
+
+let string = "Do or do not, there is no try"
+
+print(string.count)
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())

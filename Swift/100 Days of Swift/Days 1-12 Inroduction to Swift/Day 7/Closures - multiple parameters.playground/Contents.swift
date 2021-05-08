42082079b6bb9700266b4de4108bb5a20529a65a
BhargavManiar <bhargav22478@gmail.com>
2021-05-08 14:19:50 +0100

    Added Closures - multiple parameters

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 7/Closures - multiple parameters.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+
+// Two parameters for example
+func travel(action: (String, Int) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London", 60)
+    print(description)
+    print("I arrived!")
+}
+
+// Shorthand trailing closure and shorthand parameter names
+travel {
+    "I'm going to \($0) at \($1) miles per hour."
+}

78d30a69ca7f8c7f2bc35e3880dab1975cdad1ec
BhargavManiar <bhargav22478@gmail.com>
2021-05-10 16:20:10 +0100

    Added Protocol extensions

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 11/Protocol extensions.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+
+// Protocol extensions let us provide the code for a description for a method something should have.
+
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+let beatles = Set(["John", "Paul", "George", "Ringo"])
+
+extension Collection {
+    func summarize() {
+        print("There are \(count) of us:")
+        
+        for name in self {
+            print(name)
+        }
+    }
+}
+
+pythons.summarize()
+beatles.summarize()

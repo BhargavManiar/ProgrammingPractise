657ea3bdb4b6c91e402eb82a4a8493cbbd31a7ee
BhargavManiar <bhargav22478@gmail.com>
2021-05-10 14:54:53 +0100

    Added Creating classes

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 10/Creating classes.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class Dog {
+    var name: String
+    var breed: String
+    
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+
+let poppy = Dog(name: "Poppy", breed: "Poodle")
---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 10/README.md
@@ -0,0 +1,10 @@
+# Day 10 - Classes
+
+## Contents
+1. Creating your own classes
+2.  Class inheritance 
+3.  Overriding methods
+4.  Final classes
+5.  Copying objects
+6.  Deinitializers
+7. Mutability

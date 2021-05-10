6e628f26a4f092432497c816dc8f4a14e6425910
BhargavManiar <bhargav22478@gmail.com>
2021-05-10 16:22:52 +0100

    Added Protocol-oriented programming

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 11/Protocol-oriented programming.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+protocol Identifiable {
+    var id: String {get set}
+    func identify()
+}
+
+extension Identifiable {
+    func identify() {
+        print("My ID is \(id).")
+    }
+}
+
+struct User: Identifiable {
+    var id: String
+}
+
+let twostraws = User(id: "twostraws")
+twostraws.identify()

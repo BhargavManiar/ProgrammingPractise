856020bb5a747673d630aa3de377ca2f063b6812
BhargavManiar <bhargav22478@gmail.com>
2021-05-10 16:16:33 +0100

    Added Extensions

---
+++ Swift/100 Days of Swift/Days 1-12 Inroduction to Swift/Day 11/Extensions.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+// Extensions allow you to add methods to existing types.
+extension Int {
+    func squared() -> Int {
+        return self * self
+    }
+}
+
+let number = 8
+number.squared()
+
+extension Int {
+    var isEven: Bool {
+        return self % 2 == 0
+    }
+}
+
+number.isEven

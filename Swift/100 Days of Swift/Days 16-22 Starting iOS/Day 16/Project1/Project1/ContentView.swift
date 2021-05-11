9c80f5a939fe926d099527d0508f6fb52643117e
BhargavManiar <bhargav22478@gmail.com>
2021-05-11 14:47:47 +0100

    Added files for Project1

---
+++ Swift/100 Days of Swift/Days 16-22 Starting iOS/Day 16/Project1/Project1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project1
+//
+//  Created by Bhargav Maniar on 11/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift/100 Days of Swift/Days 16-22 Starting iOS/Day 16/Project1/Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Bhargav Maniar on 11/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

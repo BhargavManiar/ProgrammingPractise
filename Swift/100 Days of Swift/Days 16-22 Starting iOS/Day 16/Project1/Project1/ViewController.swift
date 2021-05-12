370fc0030f40aeed0198c96b77eda5131c72bdb7
BhargavManiar <bhargav22478@gmail.com>
2021-05-12 09:54:12 +0100

    Added more code for tableView

--- Swift/100 Days of Swift/Days 16-22 Starting iOS/Day 16/Project1/Project1/ViewController.swift
+++ Swift/100 Days of Swift/Days 16-22 Starting iOS/Day 16/Project1/Project1/ViewController.swift
@@ -25,5 +25,15 @@ class ViewController: UITableViewController {
         
         print(pictures)
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Pictures", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell        
+    }
 }
 

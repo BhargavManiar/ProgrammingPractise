ed889c5020c0b70ef96f392563d330014f486551
BhargavManiar <bhargav22478@gmail.com>
2021-05-13 11:36:33 +0100

    Added title for Storm Viewer

--- Swift/100 Days of Swift/Days 16-22 Starting iOS/Day 16-18/Project1/Project1/ViewController.swift
+++ Swift/100 Days of Swift/Days 16-22 Starting iOS/Day 16-18/Project1/Project1/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Storm Viewer"
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)

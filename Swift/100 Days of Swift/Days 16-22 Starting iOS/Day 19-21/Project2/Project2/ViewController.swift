2071fdf414948cb57ab898d3afc976bd48823eaf
BhargavManiar <bhargav22478@gmail.com>
2021-05-14 13:04:05 +0100

    Created Projec 2 Xcode Files

---
+++ Swift/100 Days of Swift/Days 16-22 Starting iOS/Day 19-21/Project2/Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Bhargav Maniar on 14/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

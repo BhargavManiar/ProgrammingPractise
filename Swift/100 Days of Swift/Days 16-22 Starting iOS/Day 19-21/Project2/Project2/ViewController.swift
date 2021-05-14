eb4b062f82b60ee7e458976af9fe7789d21a9331
BhargavManiar <bhargav22478@gmail.com>
2021-05-14 13:27:32 +0100

    Added countires array and askQuestion function for adding images to the buttons

--- Swift/100 Days of Swift/Days 16-22 Starting iOS/Day 19-21/Project2/Project2/ViewController.swift
+++ Swift/100 Days of Swift/Days 16-22 Starting iOS/Day 19-21/Project2/Project2/ViewController.swift
@@ -17,9 +17,14 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
     }
-
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 
 }
 

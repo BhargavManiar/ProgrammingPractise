c49f714ae6611ccbb0a8ea95fb952b2734e01b9e
BhargavManiar <bhargav22478@gmail.com>
2021-05-15 16:57:22 +0100

    Added code for the share menu. Added entry into plist file for saving photos to the photos app

--- Swift/100 Days of Swift/Days 16-22 Starting iOS/Day 22/Project3/Project1/DetailViewController.swift
+++ Swift/100 Days of Swift/Days 16-22 Starting iOS/Day 22/Project3/Project1/DetailViewController.swift
@@ -20,6 +20,8 @@ class DetailViewController: UIViewController {
         
         navigationItem.largeTitleDisplayMode = .never
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
@@ -33,5 +35,16 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() { // The attribute @abjc makes our code visible to objective c based UIKit code
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No Image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
 }

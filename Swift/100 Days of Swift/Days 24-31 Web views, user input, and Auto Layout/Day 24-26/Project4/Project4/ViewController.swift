99891378a4b73186e4f20340cb8a4e7017b429a4
BhargavManiar <bhargav22478@gmail.com>
2021-05-17 19:30:16 +0100

    Added code for tool bar

--- Swift/100 Days of Swift/Days 24-31 Web views, user input, and Auto Layout/Day 24-26/Project4/Project4/ViewController.swift
+++ Swift/100 Days of Swift/Days 24-31 Web views, user input, and Auto Layout/Day 24-26/Project4/Project4/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController, WKNavigationDelegate {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        toolbarItems = [spacer, refresh]
+        navigationController?.isToolbarHidden = false
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true

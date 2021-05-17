bb51ab9d08cf678f9a62d1273a200a216a07b130
BhargavManiar <bhargav22478@gmail.com>
2021-05-17 15:01:03 +0100

    Added files for Day 23 Challenge Project

---
+++ Swift/100 Days of Swift/Day 23 Consolidation 2/Challenge-Day23/Challenge-Day23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge-Day23
+//
+//  Created by Bhargav Maniar on 17/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

3344f94c993c9058ff8bd6e7e7e65bd82ad82193
BhargavManiar <bhargav22478@gmail.com>
2021-05-18 11:46:26 +0100

    Completed Challenge 2.Add back and forward buttons

--- Swift/100 Days of Swift/Days 24-31 Web views, user input, and Auto Layout/Day 24-26/Project4/Project4/ViewController.swift
+++ Swift/100 Days of Swift/Days 24-31 Web views, user input, and Auto Layout/Day 24-26/Project4/Project4/ViewController.swift
@@ -27,11 +27,14 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
         
+        let forward = UIBarButtonItem(barButtonSystemItem: .fastForward, target: webView, action: #selector(webView.goForward))
+        let back = UIBarButtonItem(barButtonSystemItem: .rewind, target: webView, action: #selector(webView.goBack))
+        
         progressView = UIProgressView(progressViewStyle: .default)
         progressView.sizeToFit()
         let progressButton = UIBarButtonItem(customView: progressView)
         
-        toolbarItems = [progressButton, spacer, refresh]
+        toolbarItems = [progressButton, spacer, back, spacer, forward, spacer, refresh]
         navigationController?.isToolbarHidden = false
         
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)

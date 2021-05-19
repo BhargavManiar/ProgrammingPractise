862cacd7631506daa86bbcb8add4efce3d4b5a61
BhargavManiar <bhargav22478@gmail.com>
2021-05-19 15:30:40 +0100

    Completed Challenge 3. Add left bar button item to restart the game with a new word

--- Swift/100 Days of Swift/Days 24-31 Web views, user input, and Auto Layout/Day 27-29/Project5/Project5/ViewController.swift
+++ Swift/100 Days of Swift/Days 24-31 Web views, user input, and Auto Layout/Day 27-29/Project5/Project5/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
@@ -30,7 +31,7 @@ class ViewController: UITableViewController {
         
     }
     
-    func startGame() {
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true) // Remove previous guesses
         tableView.reloadData()

1d40da72aae866cf6aee17afd38ad2fbcd70a71d
BhargavManiar <bhargav22478@gmail.com>
2021-05-20 12:25:24 +0100

    Commented out VFL code to demo anchors

--- Swift/100 Days of Swift/Days 24-31 Web views, user input, and Auto Layout/Day 30-31/Project6b/Project6b/ViewController.swift
+++ Swift/100 Days of Swift/Days 24-31 Web views, user input, and Auto Layout/Day 30-31/Project6b/Project6b/ViewController.swift
@@ -48,15 +48,15 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
-        let viewsDictionary = ["label1" : label1, "label2" : label2, "label3" : label3, "label4" : label4, "label5" : label5]
-        
-        for label in viewsDictionary.keys {
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary)) // H - Horizontal layout | - Edge of the view
-        }
-        
-        let metrics = ["labelHeight": 88]
-        
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+//        let viewsDictionary = ["label1" : label1, "label2" : label2, "label3" : label3, "label4" : label4, "label5" : label5]
+//
+//        for label in viewsDictionary.keys {
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary)) // H - Horizontal layout | - Edge of the view
+//        }
+//
+//        let metrics = ["labelHeight": 88]
+//
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
     }
 
 

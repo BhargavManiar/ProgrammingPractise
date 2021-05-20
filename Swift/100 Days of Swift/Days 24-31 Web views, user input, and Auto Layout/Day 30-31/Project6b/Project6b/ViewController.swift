05b6b2efd026f0293b392a5a85126e63b36ab440
BhargavManiar <bhargav22478@gmail.com>
2021-05-20 11:51:37 +0100

    Added Project6b files

---
+++ Swift/100 Days of Swift/Days 24-31 Web views, user input, and Auto Layout/Day 30-31/Project6b/Project6b/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project6b
+//
+//  Created by Bhargav Maniar on 20/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

7d9479d0252507981a6f2bca10939772bdf946ba
BhargavManiar <bhargav22478@gmail.com>
2021-05-20 14:29:06 +0100

    Added Table view functions

--- Swift/100 Days of Swift/Day 32 Consolidation 3/Challenge-Day32/Challenge-Day32/ViewController.swift
+++ Swift/100 Days of Swift/Day 32 Consolidation 3/Challenge-Day32/Challenge-Day32/ViewController.swift
@@ -19,6 +19,17 @@ class ViewController: UITableViewController {
         
     }
 
+    // Table view functions
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
 
 }
 

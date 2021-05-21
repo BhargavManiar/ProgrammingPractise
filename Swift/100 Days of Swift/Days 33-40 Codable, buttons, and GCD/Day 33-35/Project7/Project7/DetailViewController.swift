72aab23b7cd630afbd35514974337bde1785bde1
BhargavManiar <bhargav22478@gmail.com>
2021-05-21 18:13:30 +0100

    Added HTML formatting and included title in the petition

--- Swift/100 Days of Swift/Days 33-40 Codable, buttons, and GCD/Day 33-35/Project7/Project7/DetailViewController.swift
+++ Swift/100 Days of Swift/Days 33-40 Codable, buttons, and GCD/Day 33-35/Project7/Project7/DetailViewController.swift
@@ -11,6 +11,7 @@ import WebKit
 class DetailViewController: UIViewController {
     var webView: WKWebView!
     var detailItem: Petition?
+    var petitionTitle: Petition?
     
     override func loadView() {
         webView = WKWebView()
@@ -29,7 +30,8 @@ class DetailViewController: UIViewController {
         <style> body {font-size: 150%; } </style>
         </head>
         <body>
-        \(detailItem.body)
+        <h3>\(detailItem.title)</h3>
+        <p>\(detailItem.body)</p>
         </body>
         </html>
         """

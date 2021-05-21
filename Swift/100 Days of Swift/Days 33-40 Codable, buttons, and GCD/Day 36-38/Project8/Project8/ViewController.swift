dfa4ca12a322dcdd7d59deededea9bebef651ff6
BhargavManiar <bhargav22478@gmail.com>
2021-05-21 22:50:20 +0100

    Added UI properties to ViewController

--- Swift/100 Days of Swift/Days 33-40 Codable, buttons, and GCD/Day 36-38/Project8/Project8/ViewController.swift
+++ Swift/100 Days of Swift/Days 33-40 Codable, buttons, and GCD/Day 36-38/Project8/Project8/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var cluesLabel: UILabel!
+    var answersLabel: UILabel!
+    var currentAnswer: UILabel!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

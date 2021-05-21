61772fff745c8c16af38ac7d400fca0f8a1240fe
BhargavManiar <bhargav22478@gmail.com>
2021-05-21 22:46:02 +0100

    Added Xcode files and added specified device to be iPad only

---
+++ Swift/100 Days of Swift/Days 33-40 Codable, buttons, and GCD/Day 36-38/Project8/Project8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Bhargav Maniar on 21/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

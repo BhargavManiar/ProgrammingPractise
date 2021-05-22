55a03f24ebca2cba59f9e404194297b042990195
BhargavManiar <bhargav22478@gmail.com>
2021-05-22 15:51:38 +0100

    Added loadLevel function

--- Swift/100 Days of Swift/Days 33-40 Codable, buttons, and GCD/Day 36-38/Project8/Project8/ViewController.swift
+++ Swift/100 Days of Swift/Days 33-40 Codable, buttons, and GCD/Day 36-38/Project8/Project8/ViewController.swift
@@ -144,6 +144,36 @@ class ViewController: UIViewController {
         
     }
 
+    // Game functions
+    
+    func loadLevel() {
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+        
+        if let levelFileURL = Bundle.main.url(forResource: "level\(level)", withExtension: "txt") {
+            if let levelContents = try? String(contentsOf: levelFileURL) {
+                var lines = levelContents.components(separatedBy: "\n")
+                lines.shuffle()
+                
+                for (index, line) in lines.enumerated() {
+                    let parts = line.components(separatedBy: ": ")
+                    let answer = parts[0]
+                    let clue = parts[1]
+                    
+                    clueString += "\(index + 1). \(clue)\n"
+                    
+                    let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                    solutionString += "\(solutionWord.count) letters\n"
+                    solutions.append(solutionWord)
+                    
+                    let bits = answer.components(separatedBy: "|")
+                    letterBits += bits
+                }
+            }
+        }
+        
+    }
 
 }
 

7ce81229cb8af4076c0d71931541c8b9d67d7325
BhargavManiar <bhargav22478@gmail.com>
2021-05-22 20:34:15 +0100

    Added DispatchQueue for the parse function and for obtaining the data from the URL

--- Swift/100 Days of Swift/Days 33-40 Codable, buttons, and GCD/Day 33-35/Project7/Project7/ViewController.swift
+++ Swift/100 Days of Swift/Days 33-40 Codable, buttons, and GCD/Day 33-35/Project7/Project7/ViewController.swift
@@ -34,12 +34,14 @@ class ViewController: UITableViewController {
             //urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
-            
         
-        if let url = URL(string: urlString) {
-            if let data = try? Data(contentsOf: url) {
-                parse(json: data)
-                return
+        DispatchQueue.global(qos: .userInitiated).async {
+            [weak self] in
+            if let url = URL(string: urlString) {
+                if let data = try? Data(contentsOf: url) {
+                    self?.parse(json: data)
+                    return
+                }
             }
         }
         
@@ -70,7 +72,13 @@ class ViewController: UITableViewController {
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
             originalPetitions = petitions
-            tableView.reloadData()
+            
+            DispatchQueue.main.async {
+                [weak self] in
+                self?.tableView.reloadData()
+            }
+            
+            
         }
     }
     

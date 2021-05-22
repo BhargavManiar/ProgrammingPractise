f85f2e160b4947fa2e0c81be451413e10268c1f8
BhargavManiar <bhargav22478@gmail.com>
2021-05-22 14:31:17 +0100

    Added clear and submit button constraints

--- Swift/100 Days of Swift/Days 33-40 Codable, buttons, and GCD/Day 36-38/Project8/Project8/ViewController.swift
+++ Swift/100 Days of Swift/Days 33-40 Codable, buttons, and GCD/Day 36-38/Project8/Project8/ViewController.swift
@@ -72,7 +72,15 @@ class ViewController: UIViewController {
             
             currentAnswer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             currentAnswer.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
-            currentAnswer.topAnchor.constraint(equalTo: cluesLabel.bottomAnchor, constant: 20)
+            currentAnswer.topAnchor.constraint(equalTo: cluesLabel.bottomAnchor, constant: 20),
+            
+            submit.topAnchor.constraint(equalTo: currentAnswer.bottomAnchor),
+            submit.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -100),
+            submit.heightAnchor.constraint(equalToConstant: 44),
+            
+            clear.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 100),
+            clear.centerYAnchor.constraint(equalTo: submit.centerYAnchor),
+            
             
             // more constraints to be added
         ])

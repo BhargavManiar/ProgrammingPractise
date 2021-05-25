1bc1f47e54ae98620a0a3a6e674cfc0659b02c27
BhargavManiar <bhargav22478@gmail.com>
2021-05-25 18:10:35 +0100

    Added properties for the hangman game

--- Swift/100 Days of Swift/Day 41 Consolidation 4/Challenge-Day41/Challenge-Day41/ViewController.swift
+++ Swift/100 Days of Swift/Day 41 Consolidation 4/Challenge-Day41/Challenge-Day41/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var usedLetters = [String]()
+    var userInput: String = ""
+    var hiddenWord: String = ""
+    var currentWord: String = ""
+    var wrongAnswer: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

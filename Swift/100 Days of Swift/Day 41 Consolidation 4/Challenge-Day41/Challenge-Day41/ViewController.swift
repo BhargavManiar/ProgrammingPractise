4906b5f4c446947f8d677e3afa8f98b95c6521f6
BhargavManiar <bhargav22478@gmail.com>
2021-05-25 18:20:49 +0100

    Added code for file manager to obtian the words from the words.txt file

--- Swift/100 Days of Swift/Day 41 Consolidation 4/Challenge-Day41/Challenge-Day41/ViewController.swift
+++ Swift/100 Days of Swift/Day 41 Consolidation 4/Challenge-Day41/Challenge-Day41/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     var usedLetters = [String]()
+    var allWord = [String]()
     var userInput: String = ""
     var hiddenWord: String = ""
     var currentWord: String = ""
@@ -16,7 +17,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Obtain the words from file manager
+        if let wordsURL = Bundle.main.url(forResource: "words", withExtension: "txt") {
+            if let words = try? String(contentsOf: wordsURL) {
+                allWord = words.components(separatedBy: "\n")
+            }
+        }
+        
+        // Error handling if there are no items found
+        if allWord.isEmpty {
+            allWord = ["honey"]
+        }
         
         // Put a ??? title word on the nav bar
         // Add + button on the nav bar

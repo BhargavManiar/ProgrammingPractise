1fcae3f80b3b13d20107e99affdff8985534a81c
BhargavManiar <bhargav22478@gmail.com>
2021-05-26 22:38:43 +0100

    Added collectionView override functions, numberOfItems and cellForItemAt

--- Swift/100 Days of Swift/Days 42-49 A whole new world/Day 42-44/Project10/Project10/ViewController.swift
+++ Swift/100 Days of Swift/Days 42-49 A whole new world/Day 42-44/Project10/Project10/ViewController.swift
@@ -13,6 +13,18 @@ class ViewController: UICollectionViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue a PersonCell") // If this point is reached a crash will occur
+        }
+        
+        return cell
+    }
 
 
 }

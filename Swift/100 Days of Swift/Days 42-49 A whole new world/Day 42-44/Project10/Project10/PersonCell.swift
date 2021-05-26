8dc1ddbed683d9a76fd4a173774679c0ae113324
BhargavManiar <bhargav22478@gmail.com>
2021-05-26 22:32:07 +0100

    AddedUI elements with Interface Builder. Added PersonCell class and added IB outlets for label and image view to it.

---
+++ Swift/100 Days of Swift/Days 42-49 A whole new world/Day 42-44/Project10/Project10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Bhargav Maniar on 26/05/2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}

b2e7a11664a92cdc94fb84bc15dadb3d9c615abc
BhargavManiar <bhargav22478@gmail.com>
2021-05-26 14:13:32 +0100

    Removed loggers

--- Swift/100 Days of Swift/Day 41 Consolidation 4/Challenge-Day41/Challenge-Day41/ViewController.swift
+++ Swift/100 Days of Swift/Day 41 Consolidation 4/Challenge-Day41/Challenge-Day41/ViewController.swift
@@ -64,17 +64,13 @@ class ViewController: UIViewController {
     // Game Functions
     
     @objc func startGame(action: UIAlertAction) {
-        let funcName = "startGame"
-        logger(functionName: funcName, message: "Play Again")
         resetGameVariables() // rest variables
         
         if let obtainedWord = allWords.randomElement() {
             hiddenWord = obtainedWord
         }
-        logger(functionName: funcName, variableName: "hiddenWord", variableOutput: "\(String(describing: hiddenWord))")
         
         for character in hiddenWord! {
-            logger(functionName: funcName, variableName: "character", variableOutput: "\(character)")
             if currentWord != nil {
                 currentWord! += "?"
             } else {
@@ -82,22 +78,16 @@ class ViewController: UIViewController {
             }
         }
         
-        logger(functionName: funcName, variableName: "currentWord", variableOutput: "\(String(describing: currentWord))")
         title = currentWord!
         scoreTotal.text = "Score: \(score)"
     }
     
     @objc func startGame() {
-        let funcName = "startGame"
-        logger(functionName: funcName, message: "Initial Game Start")
-        
         if let obtainedWord = allWords.randomElement() {
             hiddenWord = obtainedWord
         }
-        logger(functionName: funcName, variableName: "hiddenWord", variableOutput: "\(String(describing: hiddenWord))")
         
         for character in hiddenWord! {
-            logger(functionName: funcName, variableName: "character", variableOutput: "\(character)")
             if currentWord != nil {
                 currentWord! += "?"
             } else {
@@ -105,15 +95,11 @@ class ViewController: UIViewController {
             }
         }
         
-        logger(functionName: funcName, variableName: "currentWord", variableOutput: "\(String(describing: currentWord))")
         title = currentWord!
         scoreTotal.text = "Score: \(score)"
     }
     
     @IBAction func guessLetter(_ sender: Any) {
-        let funcName = "guessLetter"
-        logger(functionName: funcName, message: "Input button pressed")
-        
         let alertController = UIAlertController(title: "Enter character", message: nil, preferredStyle: .alert)
         alertController.addTextField()
         
@@ -128,12 +114,10 @@ class ViewController: UIViewController {
     }
     
     func checkInput(_ input: String) {
-        let funcName = "checkInput"
         if input.count == 1 {
             if (!usedLetters.contains(input)) {
                 usedLetters.append(input) // Remember letter used
                 usedLettersTitle.isHidden = false
-                logger(functionName: funcName, variableName: "lettersUsed.text.count", variableOutput: "\(lettersUsed.text.count)")
                 
                 // Show letters used
                 if lettersUsed.text.count == 1 {
@@ -142,29 +126,22 @@ class ViewController: UIViewController {
                     lettersUsed.text += ", \(input)" // Add letter with comma
                 }
                 
-                logger(functionName: funcName, variableName: "usedLetters", variableOutput: "\(usedLetters)")
                 if (hiddenWord!.contains(input)) {
-                    logger(functionName: funcName, message: "Hidden letter found")
                     updateTitle(input)
                 } else {
-                    logger(functionName: funcName, message: "Hidden letter not found")
                     wrongAnswer()
                 }
             } else {
-                logger(functionName: funcName, message: "Letter reuse detected")
                 showErrorMessage(title: "Letter used already!", message: "Enter a differnet letter")
             }
         } else {
-            logger(functionName: funcName, message: "Input greater than 1")
             showErrorMessage(title: "Invalid input", message: "You need to enter one character")
         }
     }
     
     func updateTitle(_ input: String) {
-        let funcName = "updateTitle"
         var indexPosition = 0
         score += 1
-        logger(functionName: funcName, variableName: "input", variableOutput: "\(input)")
         for i in hiddenWord! {
             if String(i) == input {
                 // Update the currentWord, show where the letter is on the hideen work
@@ -176,11 +153,9 @@ class ViewController: UIViewController {
             }
             
             indexPosition += 1
-            logger(functionName: funcName, variableName: "indexPosition", variableOutput: "\(indexPosition)")
         }
         title = currentWord!
         scoreTotal.text = "Score: \(score)"
-        logger(functionName: funcName, variableName: "score", variableOutput: "\(score)")
     }
     
     func updateImageView() {
@@ -188,11 +163,9 @@ class ViewController: UIViewController {
     }
     
     func wrongAnswer() {
-        let funcName = "wrongAnswer"
         wrongAnswerScore += 1 // Increment wrong answer score
         livesRemaining.text = "Remaining Lives: \(7-wrongAnswerScore)"
         updateImageView()
-        logger(functionName: funcName, variableName: "wrongAnswerScore", variableOutput: "\(wrongAnswerScore)")
         if(wrongAnswerScore == 7) {
             endGame(win: false)
         } else {
@@ -201,16 +174,13 @@ class ViewController: UIViewController {
     }
     
     func endGame(win: Bool) {
-        let funcName = "endGame"
         var message: String
         
         if win == true {
             message = "You Won!"
-            logger(functionName: funcName, message: "Player won")
         } else {
             message = "You Lost. The word was " + hiddenWord!
             title = hiddenWord
-            logger(functionName: funcName, message: "Player lost")
         }
         
         let alertController = UIAlertController(title: "Game Over", message: message, preferredStyle: .alert)
@@ -219,52 +189,22 @@ class ViewController: UIViewController {
     }
     
     func resetGameVariables() {
-        let funcName = "resetGameVariables"
-        
-        logger(functionName: funcName, message: "Resetting Variables")
-        
         usedLetters.removeAll()
         hiddenWord = ""
         currentWord = ""
         wrongAnswerScore = 0
         hangmanImageView.image = UIImage(named: "Hang Man Drawing 0")
         
-        logger(functionName: funcName, variableName: "usedLetters", variableOutput: "\(usedLetters)")
-        logger(functionName: funcName, variableName: "hiddenWord", variableOutput: "\(String(describing: hiddenWord))")
-        logger(functionName: funcName, variableName: "currentWord", variableOutput: "\(String(describing: currentWord))")
-        logger(functionName: funcName, variableName: "wrongAnswerScore", variableOutput: "\(wrongAnswerScore)")
-        logger(functionName: funcName, variableName: "hangmanImageView", variableOutput: "\(String(describing: hangmanImageView.image))")
         // Reset UI Elements
         livesRemaining.text = "Remaining Lives: 7"
         lettersUsed.text = " "
-        
-        logger(functionName: funcName, variableName: "livesRemaining", variableOutput: "\(String(describing: livesRemaining.text))")
-        logger(functionName: funcName, variableName: "lettersUsed", variableOutput: "\(String(describing: lettersUsed.text))")
     }
     
     func showErrorMessage(title: String, message: String) {
-        let funcName = "showErrorMessage"
-        logger(functionName: funcName, variableName: "message", variableOutput: "\(message)")
         
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "OK", style: .default))
         present(alertController, animated: true)
     }
     
-    // Logger Functions
-    
-    // Messages from a function
-    func logger(functionName: String, message: String) {
-        if loggerEnable {
-            print("\(functionName): \(message)")
-        }
-    }
-    
-    // Variable output from a function
-    func logger(functionName: String, variableName: String, variableOutput: String) {
-        if loggerEnable{
-            print("\(functionName) -> \(variableName): \(variableOutput)")
-        }
-        
-    }
 }

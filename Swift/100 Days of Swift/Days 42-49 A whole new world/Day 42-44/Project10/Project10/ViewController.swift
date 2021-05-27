de948e0ff2d13a623cdd206e7999c5631394d066
BhargavManiar <bhargav22478@gmail.com>
2021-05-27 10:57:13 +0100

    Added people array property of type Person (based on Person class)

--- Swift/100 Days of Swift/Days 42-49 A whole new world/Day 42-44/Project10/Project10/ViewController.swift
+++ Swift/100 Days of Swift/Days 42-49 A whole new world/Day 42-44/Project10/Project10/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    var people = [Person]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         

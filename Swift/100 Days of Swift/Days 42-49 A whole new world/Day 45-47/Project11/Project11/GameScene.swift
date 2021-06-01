eea317e21374d078413e13812b0e305e858a1502
BhargavManiar <bhargav22478@gmail.com>
2021-06-01 19:26:54 +0100

    Added collision and destroy functions

--- Swift/100 Days of Swift/Days 42-49 A whole new world/Day 45-47/Project11/Project11/GameScene.swift
+++ Swift/100 Days of Swift/Days 42-49 A whole new world/Day 45-47/Project11/Project11/GameScene.swift
@@ -79,4 +79,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let spinForever = SKAction.repeatForever(spin)
         slotGlow.run(spinForever)
     }
+    
+    func collision(between ball: SKNode, object: SKNode) {
+        if object.name == "good" {
+            destroy(ball: ball)
+        } else if object.name == "bad" {
+            destroy(ball: ball)
+        }
+    }
+    
+    func destroy(ball: SKNode) {
+        ball.removeFromParent()
+    }
 }

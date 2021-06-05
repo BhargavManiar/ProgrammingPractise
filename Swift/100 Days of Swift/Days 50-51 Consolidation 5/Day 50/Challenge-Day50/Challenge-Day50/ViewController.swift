7432ed648f5fda9bfa059607e904b0fcb1c5092c
BhargavManiar <bhargav22478@gmail.com>
2021-06-05 22:58:36 +0100

    Changed dummy code in tableView function numberOfRowsInSection to return count of pictures array

--- Swift/100 Days of Swift/Days 50-51 Consolidation 5/Day 50/Challenge-Day50/Challenge-Day50/ViewController.swift
+++ Swift/100 Days of Swift/Days 50-51 Consolidation 5/Day 50/Challenge-Day50/Challenge-Day50/ViewController.swift
@@ -30,7 +30,7 @@ class ViewController: UITableViewController, UIImagePickerControllerDelegate, UI
 
     // Table view functions
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return pictures.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {

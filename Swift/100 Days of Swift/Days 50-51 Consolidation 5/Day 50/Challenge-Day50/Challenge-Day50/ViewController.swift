b5190899dd0812baa40ff050be4d626cf891162c
BhargavManiar <bhargav22478@gmail.com>
2021-06-05 23:42:56 +0100

    Added code for imagePickerController

--- Swift/100 Days of Swift/Days 50-51 Consolidation 5/Day 50/Challenge-Day50/Challenge-Day50/ViewController.swift
+++ Swift/100 Days of Swift/Days 50-51 Consolidation 5/Day 50/Challenge-Day50/Challenge-Day50/ViewController.swift
@@ -79,6 +79,24 @@ class ViewController: UITableViewController, UIImagePickerControllerDelegate, UI
         }
         present(picker, animated: true)
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        let picture = Pictures(name: "Unknown", image: imageName, caption: "Empty Caption")
+        pictures.append(picture)
+        save()
+        tableView.reloadData()
+        
+        dismiss(animated: true)
+    }
 
     // Data related functions
     func save() {

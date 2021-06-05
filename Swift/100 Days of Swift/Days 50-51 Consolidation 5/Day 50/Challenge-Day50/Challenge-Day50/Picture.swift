9a831c46b8c70203e77c042fb1caede39e7bf85a
BhargavManiar <bhargav22478@gmail.com>
2021-06-05 22:45:15 +0100

    Added a custom type, Picture

---
+++ Swift/100 Days of Swift/Days 50-51 Consolidation 5/Day 50/Challenge-Day50/Challenge-Day50/Picture.swift
@@ -0,0 +1,18 @@
+//
+//  Picture.swift
+//  Challenge-Day50
+//
+//  Created by Bhargav Maniar on 05/06/2021.
+//
+
+import UIKit
+
+class Picrure: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}

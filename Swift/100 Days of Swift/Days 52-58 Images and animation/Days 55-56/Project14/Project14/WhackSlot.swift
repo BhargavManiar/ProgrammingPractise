7a46316d32448213d0468b7b5e7bc24d787c9aab
BhargavManiar <bhargav22478@gmail.com>
2021-07-20 21:02:48 +0100

    Added hit function

--- Swift/100 Days of Swift/Days 52-58 Images and animation/Days 55-56/Project14/Project14/WhackSlot.swift
+++ Swift/100 Days of Swift/Days 52-58 Images and animation/Days 55-56/Project14/Project14/WhackSlot.swift
@@ -56,6 +56,15 @@ class WhackSlot: SKNode {
         
         charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
         isVisible = false
+    }
+    
+    func hit() {
+        isHit = true
         
+        let delay = SKAction.wait(forDuration: 0.25)
+        let hide = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
+        let notVisible = SKAction.run { [weak self] in self?.isVisible = false }
+        let sequenece = SKAction.sequence([delay, hide, notVisible])
+        charNode.run(sequenece)
     }
 }

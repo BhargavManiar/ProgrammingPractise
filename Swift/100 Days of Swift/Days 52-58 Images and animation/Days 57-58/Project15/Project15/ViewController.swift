6e5101d53304bc6e0cee477b5495ebfa8ce407a6
BhargavManiar <bhargav22478@gmail.com>
2021-07-21 22:00:21 +0100

    Added Xcode project files including image asset. Added button to IB and added code to ViewController

---
+++ Swift/100 Days of Swift/Days 52-58 Images and animation/Days 57-58/Project15/Project15/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Project15
+//
+//  Created by Bhargav Maniar on 21/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
+    }
+    
+    @IBAction func tapped(_ sender: Any) {
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+    
+}
+

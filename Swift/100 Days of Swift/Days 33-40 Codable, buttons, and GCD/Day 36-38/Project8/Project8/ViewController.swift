e94d1328b8bc3fd2b5e802171ba3dc0bd4761db8
BhargavManiar <bhargav22478@gmail.com>
2021-07-22 11:20:10 +0100

    Completed Project 15 Challenge 1

--- Swift/100 Days of Swift/Days 33-40 Codable, buttons, and GCD/Day 36-38/Project8/Project8/ViewController.swift
+++ Swift/100 Days of Swift/Days 33-40 Codable, buttons, and GCD/Day 36-38/Project8/Project8/ViewController.swift
@@ -146,7 +146,10 @@ class ViewController: UIViewController {
         
         currentAnswer.text = currentAnswer.text?.appending(buttonTitle)
         activatedButtons.append(sender)
-        sender.isHidden = true
+        //sender.isHidden = true
+        UIView.animate(withDuration: 1) {
+            sender.alpha = 0
+        }
     }
     
     @objc func submitTapped(_ sender: UIButton) {
@@ -186,7 +189,11 @@ class ViewController: UIViewController {
         currentAnswer.text = ""
         
         for button in activatedButtons {
-            button.isHidden = false
+            //button.isHidden = false
+            
+            UIView.animate(withDuration: 1) {
+                button.alpha = 1
+            }
         }
         
         activatedButtons.removeAll()

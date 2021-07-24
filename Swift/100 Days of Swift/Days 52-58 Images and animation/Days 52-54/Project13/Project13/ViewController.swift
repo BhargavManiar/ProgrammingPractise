7c8393a33a8a83052f41703fea274e7cc5aa77f3
BhargavManiar <bhargav22478@gmail.com>
2021-07-24 19:07:21 +0100

    Completed Project 15 Challenge 2

--- Swift/100 Days of Swift/Days 52-58 Images and animation/Days 52-54/Project13/Project13/ViewController.swift
+++ Swift/100 Days of Swift/Days 52-58 Images and animation/Days 52-54/Project13/Project13/ViewController.swift
@@ -59,6 +59,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismiss(animated: true)
         currentImage = image
         
+        imageView.alpha = 0
+        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+            [weak self] in
+            self?.currentImage = image
+            self?.imageView.alpha = 1
+        })
+        
         let beginImage = CIImage(image: currentImage)
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
         applyProcessing()

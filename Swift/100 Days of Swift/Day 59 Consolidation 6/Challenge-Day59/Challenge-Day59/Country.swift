dc464f3636a652a07800616fcd6052de663b4f75
BhargavManiar <bhargav22478@gmail.com>
2021-07-28 22:22:01 +0100

    Added Country.swift file for a custom data type

---
+++ Swift/100 Days of Swift/Day 59 Consolidation 6/Challenge-Day59/Challenge-Day59/Country.swift
@@ -0,0 +1,16 @@
+//
+//  Country.swift
+//  Challenge-Day59
+//
+//  Created by Bhargav Maniar on 28/07/2021.
+//
+
+import Foundation
+
+struct Country: Codable {
+    var countryName: String
+    var countryCapitalName: String
+    var countryPopulation: Int          // E.g 100,000 people
+    var countrySize: Int                // E.g 1,000,000 m^2
+    var countryPrimaryLanguage: String
+}

6e8c6b6a128d5cd9a68975f070d1cd43990a8b3f
BhargavManiar <bhargav22478@gmail.com>
2021-07-28 22:06:00 +0100

    Added skeleton code for the Table View functions

--- Swift/100 Days of Swift/Day 59 Consolidation 6/Challenge-Day59/Challenge-Day59/ViewController.swift
+++ Swift/100 Days of Swift/Day 59 Consolidation 6/Challenge-Day59/Challenge-Day59/ViewController.swift
@@ -14,7 +14,23 @@ class ViewController: UITableViewController {
         
         title = "Countries"
     }
+    
+    // Table View Functions
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //    let viewController = DetailViewController()
+    //    viewController.detailItem = petitions[indexPath.row]
+    //    navigationController?.pushViewController(viewController, animated: true)
+    }
 }
 

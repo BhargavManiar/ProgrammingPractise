d22cc48d976c023c53547e5beeac9d217f5b918d
BhargavManiar <bhargav22478@gmail.com>
2021-07-28 21:48:07 +0100

    Added XCode Project Files for Challenge Day 59

---
+++ Swift/100 Days of Swift/Day 59 Consolidation 6/Challenge-Day59/Challenge-Day59/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge-Day59
+//
+//  Created by Bhargav Maniar on 28/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

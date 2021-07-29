4137104b4d772e49f6a4bedd54f9affe0ca9bb6b
BhargavManiar <bhargav22478@gmail.com>
2021-07-29 20:31:29 +0100

    Imported WebKit, wrote html for the view, added loadView Controller

--- Swift/100 Days of Swift/Day 59 Consolidation 6/Challenge-Day59/Challenge-Day59/DetailViewController.swift
+++ Swift/100 Days of Swift/Day 59 Consolidation 6/Challenge-Day59/Challenge-Day59/DetailViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import WebKit
 
 class DetailViewController: UIViewController {
-
+    var webView: WKWebView!
+    var detailItem: Country?
+    
+    override func loadView() {
+            webView = WKWebView()
+            view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        guard let detailItem = detailItem else { return }
+        let html = """
+        <html>
+            <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <style> body {font-size: 150%; } </style>
+            </head>
+        
+            <body>
+            </body>
+        </html>
+        """
         // Do any additional setup after loading the view.
     }
 

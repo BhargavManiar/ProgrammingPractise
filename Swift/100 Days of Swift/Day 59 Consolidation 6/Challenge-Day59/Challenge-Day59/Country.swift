ff6672fe1276394449d5f9707ef06e02d5771b7b
BhargavManiar <bhargav22478@gmail.com>
2021-07-29 22:37:22 +0100

    Removed properties that will not be used

--- Swift/100 Days of Swift/Day 59 Consolidation 6/Challenge-Day59/Challenge-Day59/Country.swift
+++ Swift/100 Days of Swift/Day 59 Consolidation 6/Challenge-Day59/Challenge-Day59/Country.swift
@@ -11,8 +11,6 @@ struct Country: Codable {
     var countryName: String
     var countryCapitalName: String
     var countryPopulation: Int          // E.g 100,000 people
-    var countrySize: Int                // E.g 1,000,000 m^2
-    var countryPrimaryLanguage: String
 }
 
 // URL To Obtain JSON Data: https://restcountries.eu/rest/v2/all?fields=name;capital;region;currencies;population;alpha3Code

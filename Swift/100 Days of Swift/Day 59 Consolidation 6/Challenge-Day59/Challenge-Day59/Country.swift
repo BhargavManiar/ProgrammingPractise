2c8b625cdd0ea42bd7fa4098e8cbecec6e9015a9
BhargavManiar <bhargav22478@gmail.com>
2021-07-29 22:46:19 +0100

    Changed Country struct variable names to match the JSON field names

--- Swift/100 Days of Swift/Day 59 Consolidation 6/Challenge-Day59/Challenge-Day59/Country.swift
+++ Swift/100 Days of Swift/Day 59 Consolidation 6/Challenge-Day59/Challenge-Day59/Country.swift
@@ -8,9 +8,15 @@
 import Foundation
 
 struct Country: Codable {
-    var countryName: String
-    var countryCapitalName: String
-    var countryPopulation: Int          // E.g 100,000 people
+    var name: String
+    var capital: String
+    var population: Int          // E.g 100,000 people
+    var region: String           // E.g Europe
+    var currencies: [Currency]
+}
+
+struct Currency: Codable {
+    let code, name, symbol: String?
 }
 
 // URL To Obtain JSON Data: https://restcountries.eu/rest/v2/all?fields=name;capital;region;currencies;population;alpha3Code

2eded2abe194bcece885cdf8e339778ed0166f62
BhargavManiar <bhargav22478@gmail.com>
2021-07-29 20:25:27 +0100

    Added DetailViewController.swift file

---
+++ Swift/100 Days of Swift/Day 59 Consolidation 6/Challenge-Day59/Challenge-Day59/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  Challenge-Day59
+//
+//  Created by Bhargav Maniar on 29/07/2021.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
